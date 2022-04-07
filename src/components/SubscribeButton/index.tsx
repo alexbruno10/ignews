98a94e99f3e696964c9e0041d8fdac9eb33968e2
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2022-04-06 21:05:01 -0300

    Create component SubscribeButton

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+export function SubscribeButton() {
+
+    return (
+        <button type="button" className="subscribeButton">
+            Subscribe now
+        </button>
+    )
+
+}
