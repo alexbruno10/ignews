ac16d617a798fc6ba57099784641b0da9bd7e7e0
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2022-04-27 21:03:30 -0300

    Create session auth with Git Hub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,23 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+    console.log(session)
+
+    return session ? (
         
-            <button className={styles.signInButton} type="button">
+            <button className={styles.signInButton} type="button" onClick={() => signOut()}>
                 <FaGithub color="#04d361"/>
-                Alex Bruno
+                {session.user.name}
                 <FiX color="#737380" className={styles.closeIcon}/>
             </button>
         
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
             <FaGithub color="#eba417"/>
             Sign in with GitHub
         </button>
