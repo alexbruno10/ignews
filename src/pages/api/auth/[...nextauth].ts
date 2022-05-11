d070836cedd7f7255df497711dbaa56206268bc0
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2022-05-10 21:35:41 -0300

    Verify user duplicated in login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,27 +20,27 @@ export default NextAuth({
             try {
                 await fauna.query(
                     q.If(
-                        q.Not(
-                            q.Exists(
-                                q.Match(
-                                    q.Index('user_by_email'),
-                                    q.Casefold(user.email)
-                                )
-                            )
-                        ),
-                    q.Create(
-                        q.Collection('users'),
-                        { data: {email} }
-                    ),
-                    q.Get(
-                        q.Match(
+                      q.Not(
+                        q.Exists(
+                          q.Match(
                             q.Index('user_by_email'),
                             q.Casefold(user.email)
+                          )
                         )
+                      ),
+                      q.Create(
+                        q.Collection('users'),
+                        { data: { email } }
+                      ),
+                      q.Get(
+                        q.Match(
+                          q.Index('user_by_email'),
+                          q.Casefold(user.email)
+                        )
+                      )
                     )
-                    )
-                )
-                return true
+                  )
+                  return true
             } catch {
                 return false
             }
