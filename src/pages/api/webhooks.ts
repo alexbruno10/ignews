d3d97e316d75bb6ca452d3a40712656aebde00d6
Alex Bruno Neri Humberto <alex_brunoneri@hotmail.com>
2022-06-22 20:25:15 -0300

    Setting config webhook Stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-    console.log('Evento recebido')
+    console.log('Evento recebido com sucesso')
 
     res.status(200).json({ ok: true })
 }
